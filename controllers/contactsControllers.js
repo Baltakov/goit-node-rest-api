8af0c0b49ee17ce313dce2893305366a07dded5e
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-05 18:08:49 +0300

    tasks

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,77 @@
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+} from "../services/contactsServices.js";
+
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+
+export const getAllContacts = async (req, res, next) => {
+  // req ???
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    if (!contact) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await removeContact(id);
+    if (!contact) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const contact = await createContact(req.body);
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const contact = await getContactById(id, req.body);
+    if (!contact) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
