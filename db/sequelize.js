e10ad002a41f350b62167c0d1ab68b417a3fa15b
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-09 20:38:06 +0300

    transfer config to equalize

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,4 +1,23 @@
 import { Sequelize } from "sequelize";
-import { databaseConfig } from "./config.js";
+const {
+  DATABASE_DIALECT,
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = process.env;
+
+const databaseConfig = {
+  dialect: DATABASE_DIALECT,
+  database: DATABASE_NAME,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  dialectOptions: {
+    ssl: true,
+  },
+};
 
 export const sequelize = new Sequelize(databaseConfig);
