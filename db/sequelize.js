86230f05c8e31d2a45633f7cb9b67a4e9695dca6
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-09 19:00:15 +0300

    upd sequelize

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,13 +1,4 @@
 import { Sequelize } from "sequelize";
+import { databaseConfig } from "./config.js";
 
-export const sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "kostiantyn",
-  database: "db_contacts_i6lh",
-  password: "4H2mEISW5x1BioEgG2H9MCMEzvfIyTMv",
-  host: "dpg-cqqtigt6l47c73b2hk90-a.frankfurt-postgres.render.com",
-  port: "5432",
-  dialectOptions: {
-    ssl: true,
-  },
-});
+export const sequelize = new Sequelize(databaseConfig);
