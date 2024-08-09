cbdf7b1c095aca13fbb9e937f8173a5a285e9ff5
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-09 20:32:59 +0300

    add safety config.js

---
+++ db/config.js
@@ -0,0 +1,20 @@
+const {
+  DATABASE_DIALECT,
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = process.env;
+
+export const databaseConfig = {
+  dialect: DATABASE_DIALECT,
+  database: DATABASE_NAME,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  dialectOptions: {
+    ssl: true,
+  },
+};
