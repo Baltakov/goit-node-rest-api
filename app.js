81aceafda5af0f6fa50cac6c5ebd8844edae3652
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-14 23:51:38 +0300

    upd sequalize, env

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import "dotenv/config.js";
+import "dotenv/config";
 
-import { sequelize } from "./db/sequelize.js";
+import sequelize from "./db/sequelize.js";
 
 import contactsRouter from "./routes/contactsRouter.js";
-
-// dotenv.config();
 
 const app = express();
 
--- db/sequelize.js
+++ db/sequelize.js
@@ -1,16 +1,5 @@
 import { Sequelize } from "sequelize";
 
-// export const sequelize = new Sequelize({
-//   dialect: "postgres",
-//   username: "kostiantyn",
-//   database: "db_contacts_i6lh",
-//   password: "4H2mEISW5x1BioEgG2H9MCMEzvfIyTMv",
-//   host: "dpg-cqqtigt6l47c73b2hk90-a.frankfurt-postgres.render.com",
-//   port: "5432",
-//   dialectOptions: {
-//     ssl: true,
-//   },
-// });
 const {
   DATABASE_DIALECT,
   DATABASE_NAME,
@@ -19,9 +8,8 @@ const {
   DATABASE_HOST,
   DATABASE_PORT,
 } = process.env;
-console.log(DATABASE_DIALECT);
 
-const databaseConfig = {
+export const databaseConfig = {
   dialect: DATABASE_DIALECT,
   database: DATABASE_NAME,
   username: DATABASE_USER,
@@ -33,4 +21,6 @@ const databaseConfig = {
   },
 };
 
-export const sequelize = new Sequelize(databaseConfig);
+const sequelize = new Sequelize(databaseConfig);
+
+export default sequelize;
