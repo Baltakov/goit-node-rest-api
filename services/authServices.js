cea9e2453a26d55e1d78563539402dd9d8ee2887
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-18 21:47:35 +0300

    authentication & authorization

---
+++ services/authServices.js
@@ -0,0 +1,24 @@
+import express, { query } from "express";
+import bcrypt from "bcrypt";
+
+import User from "../db/models/user.js";
+import { where } from "sequelize";
+
+export const findUser = (query) =>
+  User.findOne({
+    where: query,
+  });
+
+export const signup = async (data) => {
+  try {
+    const { password } = data;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ ...data, password: hashPassword });
+    return newUser;
+  } catch (error) {
+    if (error?.parent?.code === "23505") {
+      error.message = "Email already in user";
+    }
+    throw error;
+  }
+};
