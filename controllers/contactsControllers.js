32c9e2622877fe8fcd2b1d052bdbbe4de9d9974b
Kostiantyn Baltakov <tira2500@gmail.com>
2024-08-25 13:25:03 +0300

    express

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,51 @@
+import HttpError from "../helpers/HttpError.js";
 import contactsService from "../services/contactsServices.js";
+import { tryCatchWrapper } from "../helpers/tryCatchWrapper.js";
 
-export const getAllContacts = (req, res) => {};
+const handleResult = (result) => {
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+};
 
-export const getOneContact = (req, res) => {};
+const getAllContacts = async (req, res, next) => {
+  const result = await contactsService.listContacts();
+  handleResult(result);
+  res.json(result);
+};
 
-export const deleteContact = (req, res) => {};
+const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+  handleResult(result);
+  res.json(result);
+};
 
-export const createContact = (req, res) => {};
+const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  handleResult(result);
+  res.json(result);
+};
 
-export const updateContact = (req, res) => {};
+const createContact = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const result = await contactsService.addContact(name, email, phone);
+  handleResult(result);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await contactsService.updateContact(id, req.body);
+  handleResult(result);
+  res.json(result);
+};
+
+export default {
+  getAllContacts: tryCatchWrapper(getAllContacts),
+  getOneContact: tryCatchWrapper(getOneContact),
+  deleteContact: tryCatchWrapper(deleteContact),
+  createContact: tryCatchWrapper(createContact),
+  updateContact: tryCatchWrapper(updateContact),
+};
