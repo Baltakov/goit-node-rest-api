0c3b169aa3b46c5b442dd5ba3a3f66e5a5bf8080
Kostiantyn Baltakov <tira2500@gmail.com>
2024-09-03 23:08:20 +0300

    sendEmail

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
+import nodemailer from "nodemailer";
 
 import sequelize from "./db/sequelize.js";
 
@@ -9,6 +10,32 @@ import authRouter from "./routes/authRouter.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
+
+const { UKR_NET_PASSWORD, UKR_NET_FROM } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.ukr.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: UKR_NET_FROM,
+    pass: UKR_NET_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const email = {
+  from: UKR_NET_FROM,
+  to: "momayew401@janfab.com",
+  subject: "Test email",
+  html: "<strong>Test email</strong>",
+};
+
+transport
+  .sendMail(email)
+  .then(() => console.log("Email send success"))
+  .catch((error) => console.log(error.message));
 
 app.use(morgan("tiny"));
 app.use(cors());
